93e98b37ca28836c39129db02412666b3dac53dc
ifaz5 <mutasim.ifaz@gmail.com>
2022-07-30 21:09:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt="" />
+      <img
+        style={{ height: "250px" }}
+        src="https://i.ibb.co/0KJJMqR/manifest-json.png"
+        alt=""
+      />
+      <img
+        style={{ height: "250px" }}
+        src="https://i.ibb.co/5Wv3Hqw/index-html.png"
+        alt=""
+      />
+      <h1>
+        Your web app is a progressive web app (PWA). change in the manifest.json
+        file for your own data.
+      </h1>
     </div>
   );
 }
